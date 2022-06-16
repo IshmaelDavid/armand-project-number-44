9271696519fcbd51d1bf8458d926f63346921944
Ishmael David DUSHIMIMANA <dishmaeldavid@gmail.com>
2022-06-16 19:07:09 +0200

    Final touch

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function(){
 	loader.style.display = 'none';
 })
 
+// FUNCTION THAT CONTROLS BACK TO TOP BUTTON
 function toTop() {
     window.scrollTo(0, 0);
 }
@@ -13,6 +14,7 @@ window.onscroll = function(){
     disappearScroll();
 }
 
+// FUNCTION THAT BACK TO TOP BUTTON DISAPPEAR WHEN THE PAGE VIEW IS CLOSE TO TOP
 function disappearScroll(){
     if(document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
         scrollBlock.style.display="block";
@@ -22,7 +24,7 @@ function disappearScroll(){
     }
 }
 
-
+// THE FUNCTION THAT TOGGLES HORIZONTAL MENU WITH TOGGLE DEPENDING ON THE SCREEN, ABD MAKE BURGER MENU SLIDES FROM RIGHT.
 const navSlide = () => {
 	//burger sign
 	const burger = document.querySelector('.burger');
@@ -53,11 +55,13 @@ const navSlide = () => {
 
 navSlide();
 
+// A FUNCTION THAT OPENS UP POPUP FORM WHEN A BUTTON IS CLICKED
 function openPopUp(){
 	document.getElementById('contactForm').reset();
 	document.getElementById("popUpForm").classList.remove("hide");
 }
 
+// A FUNCTION THAT CLOSES POPUP FORM WHEN A CERTAIN BUTTON IS CLICKED OR WHEN THE FORM IS SUCCESSFULLY SUBMITTED
 function hidePopUp(){
 	document.getElementById("popUpForm").classList.add("hide");
 	document.getElementById("contactForm").reset();
@@ -69,6 +73,7 @@ const indicators = [...document.querySelectorAll('.indicators button')];
 
 let currentTestimonial = 0;
 
+// A FUNCTION THAT CONTROLS THE SLIDING EFFECT OF TESTIMONIES SECTION
 indicators.forEach((item, i) => {
 	item.addEventListener('click' , () =>{
 		indicators[currentTestimonial].classList.remove('active');
@@ -77,3 +82,5 @@ indicators.forEach((item, i) => {
 		currentTestimonial = i;
 	})
 })
+
+// END OF FILE
